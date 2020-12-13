b1df7a9e334e6a700e694dd6bf3637895aa70a03
David <david.mael.cormier@gmail.com>
2020-12-13 05:25:09 +0000

    still broken

--- fsm.vhdl
+++ fsm.vhdl
@@ -3,43 +3,47 @@ use ieee.std_logic_1164.all;
 
 entity fsm is
     port (
-        sss: out std_logic_vector (1 downto 0);
-        ccc: out bit
+        state: out integer
     );
 end fsm;
 
 architecture behave of fsm is
     constant clk_c : time := 10 ns;
     signal clk : bit;
-    signal state : std_logic_vector (1 downto 0);
+    signal statelocal : integer;
 begin
     clk_process: process
     begin
         clk <= '0';
-        ccc <= '0';
         wait for clk_c/2;
         clk <= '1';
-        ccc <= '1';
         wait for clk_c/2;
+
+        if NOW > 100 ns then
+            wait;
+        end if;
     end process; 
+
+    state_process: process 
+    begin
+        state <= statelocal;
+        report integer'image(statelocal);
+    end process;
+
 
     fsm_process: process (clk)
     begin
         --should be no need to check again for clk edge
         if clk = '1' then
-            case state is
-                when "00" => 
-                    state <= "01";
-                    sss <= "01";
-                when "01" => 
-                    state <= "10";
-                    sss <= "10";
-                when "10" => 
-                    state <= "00";
-                    sss <= "00";
+            case statelocal is
+                when 0 => 
+                    statelocal <= 1;
+                when 1 => 
+                    statelocal <= 2;
+                when 2 => 
+                    statelocal <= 0;
                 when others => 
-                    state <= "00";
-                    sss <= "00";
+                    statelocal <= 0;
             end case; 
         end if;
     end process; 
