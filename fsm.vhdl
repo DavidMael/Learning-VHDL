b38e16a2ef3710780ccf942705b09dd804d50572
David <david.mael.cormier@gmail.com>
2020-12-15 18:00:58 +0000

    fsm with state type and internal clock

--- fsm.vhdl
+++ fsm.vhdl
@@ -3,31 +3,24 @@ use ieee.std_logic_1164.all;
 
 entity fsm is
     port (
-        state: out integer
+        --clk: in bit;
+        state: out std_logic_vector (1 downto 0)
     );
 end fsm;
 
-architecture behave of fsm is
-    constant clk_c : time := 10 ns;
-    signal clk : bit;
-    signal statelocal : integer;
+architecture behave_fsm of fsm is
+    type state_type is (one,two,three);
+    signal statelocal : state_type;
+    signal clk: std_logic;
+    constant clk_period : time := 10 ns;
 begin
+
     clk_process: process
     begin
         clk <= '0';
-        wait for clk_c/2;
+        wait for clk_period/2;
         clk <= '1';
-        wait for clk_c/2;
-
-        if NOW > 100 ns then
-            wait;
-        end if;
-    end process; 
-
-    state_process: process 
-    begin
-        state <= statelocal;
-        report integer'image(statelocal);
+        wait for clk_period/2;
     end process;
 
 
@@ -36,18 +29,25 @@ begin
         --should be no need to check again for clk edge
         if clk = '1' then
             case statelocal is
-                when 0 => 
-                    statelocal <= 1;
-                when 1 => 
-                    statelocal <= 2;
-                when 2 => 
-                    statelocal <= 0;
+                when one => 
+                    statelocal <= two;
+                    state <= "10";
+                when two => 
+                    statelocal <= three;
+                    state <= "11";
+                when three => 
+                    statelocal <= one;
+                    state <= "01";
                 when others => 
-                    statelocal <= 0;
+                    statelocal <= one;
+                    state <= "01";
             end case; 
         end if;
     end process; 
-end behave;
+
+end behave_fsm;
+
+
 
 
 
