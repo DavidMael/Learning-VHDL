2f29b95fa947cd2502378a52d0d2781ab1a4701e
David <david.mael.cormier@gmail.com>
2020-12-18 02:18:12 +0000

    mem read from file won't work

--- fsm_mem.vhdl
+++ fsm_mem.vhdl
@@ -2,6 +2,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use STD.textio.all;
+--use ieee.std_logic_textio.all;
 
 entity fsm_rom is
     port(
@@ -10,12 +12,42 @@ entity fsm_rom is
     );
 end fsm_rom;
 
-architecture rom_arch of fsm_rom is
+--architecture rom_arch of fsm_rom is
+--    type mem_array is array (0 to 3) of std_logic_vector (7 downto 0);
+--    constant rom_content: mem_array := ("00000000",
+--                                        "01000000",
+--                                        "00001000",
+--                                        "10001000");
+--begin
+--    data <= rom_content(to_integer(unsigned(addr)));
+--end rom_arch; 
+
+architecture rom_load of fsm_rom is
     type mem_array is array (0 to 3) of std_logic_vector (7 downto 0);
-    constant rom_content: mem_array := ("00000000",
-                                        "01000000",
-                                        "00001000",
-                                        "10001000");
+    signal rom: mem_array;
+    type line is access string;
+    type text is file of string;
+    file load_file: text;
 begin
-    data <= rom_content(to_integer(unsigned(addr)));
-end rom_arch; 
+    mem_init: process
+        variable inbuf: line;
+        --file load_file: text open read_mode is "mem_file.load";
+        variable num_words: integer := 0;
+        variable word: string (1 to 8);
+    begin
+        file_open(load_file, "mem_file.txt");
+        while not endfile(load_file) loop
+            readline(load_file, word);
+            --read(inbuf, word);
+            rom(num_words) <= to_stdlogicvector(word);
+            num_words := num_words + 1;
+            if num_words > 4 then
+                exit;
+            end if;
+        end loop;
+        wait;
+    end process;
+
+    data <= rom(to_integer(unsigned(addr)));
+
+end rom_load; 
